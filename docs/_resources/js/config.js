8037f4357e2a0b825e0793e9ea34e21d688f6139
Richard Sprague <richardsprague@gmail.com>
2025-06-05 15:25:53 -0700

    with config.js

---
+++ docs/_resources/js/config.js
@@ -0,0 +1,4 @@
+const stripeConfig = {
+  publishableKey: 'pk_live_51MPasYFVqC8fbxB6LV4faiSeA6uH7VjPt5NbLO78GkRTF91mQtO7AU20Q0WEWVJHeQUM8W3XEmkkSjrT9rONWraw00gSqGIzrJ', // Replace with live key
+  priceId: 'price_1RUa4cFVqC8fbxB6WWtakog4', // Replace with live price ID
+};
